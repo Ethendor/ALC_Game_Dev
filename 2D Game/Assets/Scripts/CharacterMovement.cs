6e33d68ef9d88b5f12e874add8251a6b8b7a2331
Darien <darien.legates@gmail.com>
2018-09-12 08:09:16 -0600

    added jump script

--- 2D Game/Assets/Scripts/CharacterMovement.cs
+++ 2D Game/Assets/Scripts/CharacterMovement.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Player Movement Variable
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    // Player Grounded Variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool grounded;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This code makes the character jump
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();   
+        }
+    }
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+
+
 }
