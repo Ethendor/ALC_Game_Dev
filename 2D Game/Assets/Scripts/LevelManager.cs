74513839e4312b1d9caf04f9abed844c030d8972
Darien <darien.legates@gmail.com>
2018-09-28 10:27:23 -0600

    Added Checkpoint Script, Halfway through LevelManager Script

---
+++ 2D Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+    private Rigidbody2D pc;
+
+    // Particles
+
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+
+    // Respawn Delay
+
+    public float respawnDelay;
+
+    // Point Peanalty for Death
+
+    public int pointPenaltyOnDeath;
+
+    // Store Gravity Value
+
+    private float gravityStore;
+
+	// Use this for initialization
+	void Start () {
+        pc = FindObjectOfType<Rigidbody2D>();
+	}
+	
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo(){
+        // Generate Death Particle
+        Instantiate(deathParticle, pc.transform.position, pc.transfrom.rotation);
+        // Hide Player
+        pc.enabled = false;
+        pc.GetComponent<Renderer>().enabled = false;
+        // Gravity Reset
+        gravityStore = pc.GetComponent<Rigidbody2D>().gravityScale;
+        pc.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        pc.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        // Point Penalty
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+        // Debug Message
+        Debug.Log("Player Respawn");
+        // Respawn Delay
+        yield return new WaitForSeconds 
+    }
+}
