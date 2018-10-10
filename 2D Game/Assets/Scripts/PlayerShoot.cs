c56e4702ad2e5d24f9861ed53c81721b0e0b9a86
Darien <darien.legates@gmail.com>
2018-10-10 10:28:22 -0600

    Added a projectile that kills the enemy

---
+++ 2D Game/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Transform FirePoint;
+    public GameObject Projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
+		
+	}
+}
